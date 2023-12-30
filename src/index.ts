c03f1eaf97f06496e0450a038056a96bea166c30
Alif Mufthi <alif.mufthi@gmail.com>
2023-12-30 14:40:19 +0700

    basic func

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,6 @@
-import { bool, Canister, nat, nat64, query, Record, StableBTreeMap, text, update, Void, Vec, nat8, Opt, AzleNull, None, AzleOpt, Some, AzleNat64 } from 'azle';
+import { Canister, nat64, query, Record, StableBTreeMap, text, update, Void, Vec, nat32, Opt, None, Some, nat8 } from 'azle';
 
 // Defining record types for different entities
-
-const Room = Record({
-    id: nat64,
-    doctor_id: nat64,
-    current_patient_id: Opt(nat64),
-})
-
-type Room = typeof Room.tsType
-
-const Doctor = Record({
-    id: nat64,
-    name: text,
-    is_available: bool,
-})
-
-type Doctor = typeof Doctor.tsType
-
 const Patient = Record({
     id: nat64,
     name: text,
@@ -28,37 +11,25 @@ const Patient = Record({
 
 type Patient = typeof Patient.tsType
 
-type RoomQueue = {
-    [room_id: number]: number[];
-};
+const Appointment = Record({
+    date: nat32,
+    patients: Vec(nat64),
+    quota_left: nat32,
+})
 
-type DoctorRoom = {
-    [doctor_id: number]: number;
-}
+type Appointment = typeof Appointment.tsType
+
 
 // This is a global variable that is stored on the heap
 
-let patientStorage = StableBTreeMap<nat64, Patient>(0);
-let doctorStorage = StableBTreeMap<nat64, Doctor>(1);
-let roomStorage = StableBTreeMap<nat64, Room>(2);
+let quotaConfiguration = StableBTreeMap<string, number>(0);
+let patientStorage = StableBTreeMap<nat64, Patient>(1);
+let appointmentStorage = StableBTreeMap<nat32, Appointment>(2)
 
-let roomQueue: RoomQueue = {};
-let doctorRoom: DoctorRoom = {};
+// constant
+const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 export default Canister({
-
-    registerDoctor: update([text], Void, (name) => {
-        const id = doctorStorage.len()
-        doctorStorage.insert(id, {
-            id,
-            name,
-            is_available: false,
-        })
-    }),
-
-    getDoctors: query([], Vec(Doctor), () => {
-        return doctorStorage.values();
-    }),
 
     registerPatient: update([text, nat8, text, text], Void, (name, age, gender, blood_type) => {
         const id = patientStorage.len()
@@ -75,102 +46,67 @@ export default Canister({
         return patientStorage.values();
     }),
 
-    registerRoom: update([nat64], Void, (doctor_id) => {
-        const id = roomStorage.len()
-        roomStorage.insert(id, {
-            id,
-            doctor_id,
-            current_patient_id: None,
-        })
+    getQuotaConfig: query([text], nat32, (day) => {
+        const result = quotaConfiguration.get(day)
+        if (result == None) {
+            return 0
+        }
+        return result.Some!
     }),
 
-    checkinDoctor: update([nat64], Void, (doctor_id) => {
-        const getDoctor = doctorStorage.get(doctor_id)
-        if (getDoctor.Some) {
-            const doctor = getDoctor.Some!
-            doctor.is_available = true
-            doctorStorage.insert(doctor_id, doctor)
-            const room = roomStorage.values().find((room) => {
-                return room.doctor_id == doctor_id
-            })
-            if (room) {
-                roomQueue[Number(room.id)] = []
+    setQuotaConfig: update([text, nat32], Void, (day, quota) => {
+        quotaConfiguration.insert(day, quota)
+    }),
+
+    getQuota: query([nat32], nat32, (date_unix) => {
+        const result = appointmentStorage.get(date_unix)
+        if (result == None) {
+            const date = new Date(date_unix * 1000)
+            const day = days[date.getDay()]
+            const quotaConfig = quotaConfiguration.get(day)
+            if (quotaConfig == None) {
+                return 0
             }
+            return quotaConfig.Some!
         }
+        return result.Some?.quota_left!
     }),
 
-    checkoutDoctor: update([nat64], Opt(text), (doctor_id) => {
-        const doctor = doctorStorage.get(doctor_id).Some
-        if (doctor) {
-            const room = roomStorage.values().find((room) => {
-                return room.doctor_id === doctor_id
-            })
-            if (room) {
-                if (roomQueue[Number(room.id)].length === 0) {
-                    doctor.is_available = false
-                    doctorStorage.insert(doctor_id, doctor)
-                    return None
-                }
-                return Some("queue still have patient")
+    makeAppointment: update([nat64, nat32], Opt(text), (patient_id, date_unix) => {
+        const patient = patientStorage.get(patient_id)
+        if (patient == None) {
+            return Some("patient not found")
+        }
+        const appointment = appointmentStorage.get(date_unix)
+        if (appointment == None) {
+            const date = new Date(date_unix * 1000)
+            const day = days[date.getDay()]
+            const quota = quotaConfiguration.get(day)
+            if (quota == None) {
+                return Some("quota of day not set")
             }
-            return Some("room is not found")
-        }
-        return Some("doctor is not found")
-    }),
-
-    allocatePatient: update([nat64, nat64], Opt(text), (patient_id, room_id) => {
-        const room = roomStorage.get(room_id).Some
-        if (room) {
-            const doctor = doctorStorage.get(room.doctor_id).Some
-            if (doctor) {
-                if (doctor.is_available) {
-                    room.current_patient_id = Some(patient_id)
-                    roomQueue[Number(room_id)].push(Number(patient_id))
-                    return None
-                }
-                return Some("doctor is not available")
+            const newAppointment = {
+                date: date_unix,
+                patients: new BigUint64Array(1),
+                quota_left: quota.Some! - 1,
             }
-            return Some("doctor is not found")
+            newAppointment.patients.set([patient_id], 0)
+            appointmentStorage.insert(date_unix, newAppointment)
+            return None
         }
-        return Some("room is not found")
-    }),
-
-    nextPatient: update([nat64], Opt(text), (room_id) => {
-        const room = roomStorage.get(room_id).Some
-        if (room) {
-            const doctor = doctorStorage.get(room.doctor_id).Some
-            if (doctor) {
-                if (doctor.is_available) {
-                    roomQueue[Number(room_id)].shift()
-                    if (roomQueue[Number(room_id)].length > 0) {
-                        AzleNat64
-                        room.current_patient_id = Some(BigInt(roomQueue[Number(room_id)][0]))
-                        return None
-                    }
-                    return Some("queue is empty")
-                }
-                return Some("doctor is not available")
-            }
-            return Some("doctor is not found")
+        const updateAppointment = appointment.Some!
+        if (updateAppointment.patients.includes(patient_id)) {
+            return Some("patient already registered on that date")
         }
-        return Some("room is not found")
-    }),
-
-    queueLength: query([nat64], nat64, (room_id) => {
-        return BigInt(roomQueue[Number(room_id)].length)
-    }),
-
-    checkAllocation: query([nat64], Opt(nat64), (patient_id) => {
-        const queues = Object.values(roomQueue)
-        const keys = Object.keys(roomQueue)
-        const idx = queues.findIndex((queue) => {
-            return queue.includes(Number(patient_id))
-        })
-        if (idx >= 0) {
-            return Some(BigInt(keys[idx]))
+        if (updateAppointment.quota_left < 1) {
+            return Some("there is no quota left available")
         }
+        const newPatients = new BigUint64Array(updateAppointment.patients.length + 1)
+        newPatients.set([...updateAppointment.patients, patient_id])
+        updateAppointment.patients = newPatients
+        updateAppointment.quota_left--
+        appointmentStorage.insert(updateAppointment.date, updateAppointment)
         return None
-    }),
-
+    })
 });
 
